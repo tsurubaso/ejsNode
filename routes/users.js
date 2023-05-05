7b58713cad3bf4428a193edb5abde9232dd12a3b
LAPTOP-L82MDLB4\user <tsurubaso@gmail.com>
2023-05-06 04:06:43 +0900

    yahooo

--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,14 @@ router.get("/new", (req, res) => {
   res.send("User new form");
 });
 
+router.post('/',(req, res) => {
+
+  res.send('Create User')
+});
+
+router.get('/:id',(req, res) => {
+req.params.id;
+res.send(`Get user with Id: ${req.params.id}`)
+})
+
 module.exports=router
