61042cb8decacdcba866584282847caf5c61976a
tsurubaso <tsurubaso@gmail.com>
2023-05-08 12:23:40 +0900

    Some update have to go next to here https://www.youtube.com/watch?v=4r6WdaY3SOA&list=PLZlA0Gpn_vH8jbFkBjOuFjhxANC63OmXM&index=2

--- routes/users.js
+++ routes/users.js
@@ -14,9 +14,27 @@ router.post('/',(req, res) => {
   res.send('Create User')
 });
 
+/*
 router.get('/:id',(req, res) => {
-req.params.id;
+//req.params.id;
+//always put static routes above
 res.send(`Get user with Id: ${req.params.id}`)
 })
+*/
+const users = [{name:"bla"},{name:"blub"}];
+router.param("id", (req, res, next, id) => {
+req.user=users[id];
+next();
+})
+
+router.route("/:id")
+.get((req, res) => {
+  console.log(req.user);
+  res.send('1')})
+.put((req, res) => {res.send('2')})
+.delete((req, res) => {res.send('3')})
+
+
+
 
 module.exports=router
